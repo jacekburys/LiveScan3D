db9770b42a94dc81ccd53f4f5246bffd96cf1224
adamhosier <hosieradam@gmail.com>
2016-10-27 15:13:39 +0100

    added code to convert livescan image representation to opencv

--- src/LiveScanClient/arucoCalibration.cpp
+++ src/LiveScanClient/arucoCalibration.cpp
@@ -13,8 +13,9 @@
 //        year={2015},
 //    }
 
-
 #include "arucoCalibration.h"
+#include "opencv2/aruco/charuco.hpp"
+
 ArucoCalibration::ArucoCalibration()
 {
 	bCalibrated = false;
@@ -35,5 +36,31 @@ ArucoCalibration::~ArucoCalibration()
 
 bool ArucoCalibration::Calibrate(RGB *pBuffer, Point3f *pCameraCoordinates, int cColorWidth, int cColorHeight)
 {
+	// Convert RGB array [pBuffer] into opencv image
+	// TODO: Maybe implement this earlier (before this function is called), 
+	//       or by changing how the image is read from the camrea?
+	cv::Mat img(cColorWidth, cColorHeight, CV_8UC3, cv::Vec3b(0,0,0));
+	for (int y = 0; y < cColorHeight; y++)
+	{
+		for (int x = 0; x < cColorWidth; x++)
+		{
+			// get pixel at (x, y) 
+			RGB pixel = pBuffer[cColorWidth * y + x];
+			// set the img map at x, y to the rgb specified in pbuffer
+			img.at<cv::Vec3b>(cv::Point(x, y)) = cv::Vec3b(pixel.rgbRed, pixel.rgbGreen, pixel.rgbBlue);
+		}
+	}
+
+	// DICT_ARUCO_ORIGINAL uses 5x5 markers afaik
+	cv::aruco::Dictionary* dict = cv::aruco::getPredefinedDictionary(cv::aruco::DICT_ARUCO_ORIGINAL);
+
+	// output structures
+	vector<vector<Point2f>> corners;
+	vector<int> ids;
+	
+	// find markers in img, store corners in corners and ids in ids
+	cv::aruco::detectMarkers(img, dict, corners, ids);
+
+	cv::aruco::drawDetectedMarkers(img, corners, ids);
 	return true;
 }
